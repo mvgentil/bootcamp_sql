34fed7eb49c1374dfbd81cebffa9b874ec17f48c
Matheus Gentil <matheusvgentil@hotmail.com>
2024-08-17 15:57:11 -0300

    aula 3

---
+++ aula04_sql/desafio.sql
@@ -0,0 +1,60 @@
+-- Faça a classificação dos produtos mais venvidos usando usando RANK(), DENSE_RANK() e ROW_NUMBER()
+-- Essa questão tem 2 implementações, veja uma que utiliza subquery e uma que não utiliza.
+-- Tabelas utilizadas FROM order_details o JOIN products p ON p.product_id = o.product_id;
+SELECT  
+  o.order_id, 
+  p.product_name, 
+  (o.unit_price * o.quantity) AS total_sale,
+  ROW_NUMBER() OVER (ORDER BY (o.unit_price * o.quantity) DESC) AS order_rn, 
+  RANK() OVER (ORDER BY (o.unit_price * o.quantity) DESC) AS order_rank, 
+  DENSE_RANK() OVER (ORDER BY (o.unit_price * o.quantity) DESC) AS order_dense
+FROM  
+  order_details o
+JOIN 
+  products p ON p.product_id = o.product_id;
+
+SELECT  
+  sales.product_name, 
+  total_sale,
+  ROW_NUMBER() OVER (ORDER BY total_sale DESC) AS order_rn, 
+  RANK() OVER (ORDER BY total_sale DESC) AS order_rank, 
+  DENSE_RANK() OVER (ORDER BY total_sale DESC) AS order_dense
+FROM (
+  SELECT 
+    p.product_name, 
+    SUM(o.unit_price * o.quantity) AS total_sale
+  FROM  
+    order_details o
+  JOIN 
+    products p ON p.product_id = o.product_id
+  GROUP BY p.product_name
+) AS sales
+ORDER BY order_rn;
+
+
+-- Listar funcionários dividindo-os em 3 grupos usando NTILE
+-- FROM employees;
+SELECT first_name, last_name, title,
+   NTILE(3) OVER (ORDER BY first_name) AS group_number
+FROM employees;
+
+
+-- Ordenando os custos de envio pagos pelos clientes de acordo 
+-- com suas datas de pedido, mostrando o custo anterior e o custo posterior usando LAG e LEAD:
+-- FROM orders JOIN shippers ON shippers.shipper_id = orders.ship_via;
+SELECT 
+  customer_id, 
+  TO_CHAR(order_date, 'YYYY-MM-DD') AS order_date, 
+  shippers.company_name AS shipper_name, 
+  LAG(freight) OVER (PARTITION BY customer_id ORDER BY order_date DESC) AS previous_order_freight, 
+  freight AS order_freight, 
+  LEAD(freight) OVER (PARTITION BY customer_id ORDER BY order_date DESC) AS next_order_freight
+FROM 
+  orders
+JOIN 
+  shippers ON shippers.shipper_id = orders.ship_via;
+
+-- Desafio extra: questão intrevista Google
+-- https://medium.com/@aggarwalakshima/interview-question-asked-by-google-and-difference-among-row-number-rank-and-dense-rank-4ca08f888486#:~:text=ROW_NUMBER()%20always%20provides%20unique,a%20continuous%20sequence%20of%20ranks.
+-- https://platform.stratascratch.com/coding/10351-activity-rank?code_type=3
+-- https://www.youtube.com/watch?v=db-qdlp8u3o
